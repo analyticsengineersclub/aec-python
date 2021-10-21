68da9265d07a101d72bb03fcba433489213c2d41
Michael Ferguson <ubuntu@ip-172-31-18-227.us-east-2.compute.internal>
2021-10-21 20:52:12 +0000

    reformatted using black

--- tests/subtract_test.py
+++ tests/subtract_test.py
@@ -1,17 +1,19 @@
 import unittest
 from math import aec_subtract
 
+
 class TestSubtract(unittest.TestCase):
+    def test_subtract(self):
+        arg_ints = [20, 5]
+        sub_result = aec_subtract(arg_ints)
+        self.assertEqual(sub_result, 15)
 
-	def test_subtract(self):
-		arg_ints = [20, 5]
-		sub_result = aec_subtract(arg_ints)
-		self.assertEqual(sub_result, 15)
+        # def test_cant_go_below_zero(self):
 
-    # def test_cant_go_below_zero(self):
-	# 	arg_ints = [5, 20]
-	# 	sub_result = aec_subtract(arg_ints)
-	# 	self.assertEqual(sub_result, 0)
+    # 	arg_ints = [5, 20]
+    # 	sub_result = aec_subtract(arg_ints)
+    # 	self.assertEqual(sub_result, 0)
 
-if __name__ == '__main__':
-	unittest.main()
+
+if __name__ == "__main__":
+    unittest.main()
